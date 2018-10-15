46a6498638ab13b3a9be6920c547f454d77cc7b8
Nicolas van de Walle <dev.nw@walle.be>
2018-10-15 15:07:11 +0200

    Added the real_address code + sources of inspiration (github)

--- src/socket/real_address.c
+++ src/socket/real_address.c
@@ -6,6 +6,8 @@
  * NOMA    : 59101600           &   27901600
  *
  * Contenu repris et complete de l'exercice preparatoire au projet : https://inginious.info.ucl.ac.be/course/LINGI1341/envoyer-et-recevoir-des-donnees
+ * Réalisé avec l'aide des sites suivants : https://github.com/Donaschmi/LINGI1341/blob/master/Inginious/Envoyer_et_recevoir_des_donn%C3%A9es/real_address.c
+
  */
 
 #include <netinet/in.h> // sockaddr_in6
@@ -21,6 +23,37 @@
  *          (const char* means the caller cannot modify or free the return value,
  *           so do not use malloc!)
  */
+
+
 const char * real_address(const char *address, struct sockaddr_in6 *rval) {
-    // TODO
+    if (address == NULL) {
+        return "Parameter \"address\" should not be NULL";
+    }
+    if (rval == NULL) {
+        return "Parameter \"rval\" should not be NULL";
+    }
+
+    struct addrinfo addressInfo;
+
+    memset(&addressInfo, 0, sizeof(struct addrinfo));
+
+    addressInfo->ai_family = AF_INET6;
+    addressInfo->ai_socktype = SOCK_DGRAM;
+    addressInfo->ai_protocol = IPPROTO_UDP;
+    addressInfo->ai_flags = 0;
+
+
+    struct addrinfo *tmp;
+    int addrinfo = getaddrinfo(address, NULL, &addressInfo, &tmp);
+    if (addrinfo != 0) {
+        return "getaddrinfo returned a non NULL value";
+    }
+
+    memcpy(rval, (struct sockaddr_in6*) tmp->ai_addr, sizeof(struct sockaddr_in6*));
+
+    freeaddrinfo(tmp);
+
+    return NULL;
+
+
 }
