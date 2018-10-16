2d26e6812a39a05e62bee0eaae20b8daaae83de4
Nicolas van de Walle <dev.nw@walle.be>
2018-10-16 11:54:50 +0200

    Added .h files for socket part

---
+++ src/socket/real_address.h
@@ -0,0 +1,24 @@
+/**
+ * Projet1 - Reseaux Informatiques
+ * Octobre 2018
+ *
+ * Auteurs : Brieuc de Voghel   &   Nicolas van de Walle
+ * NOMA    : 59101600           &   27901600
+ *
+ * Contenu repris et complete de l'exercice preparatoire au projet : https://inginious.info.ucl.ac.be/course/LINGI1341/envoyer-et-recevoir-des-donnees
+ * Réalisé avec l'aide des sites suivants : https://github.com/Donaschmi/LINGI1341/blob/master/Inginious/Envoyer_et_recevoir_des_donn%C3%A9es/real_address.c
+ *                                          https://stackoverflow.com/questions/20411223/warning-passing-argument-2-of-getsockname-from-incompatible-pointer-type
+ */
+
+/**
+ * Resolve the resource name to an usable IPv6 address
+ * @address: The name to resolve
+ * @rval: Where the resulting IPv6 address descriptor should be stored
+ * @return: NULL if it succeeded, or a pointer towards
+ *          a string describing the error if any.
+ *          (const char* means the caller cannot modify or free the return value,
+ *           so do not use malloc!)
+ */
+
+
+const char * real_address(const char *address, struct sockaddr_in6 *rval);
