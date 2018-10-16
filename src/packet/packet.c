042197952796b68aebc6b099430265b711f25fcb
Brieuc de Voghel <bdev@telenet.be>
2018-10-16 23:20:48 +0200

    Tests of packet moved as comment to test1.c for later CUnit implementation

--- src/packet/packet.c
+++ src/packet/packet.c
@@ -37,57 +37,7 @@ void pkt_del(pkt_t *pkt) {
     free(pkt);
 }
 
-/**
- * For testing only
- * @param argc
- * @param argv
- * @return
- */
-int main(int argc, char* argv[]) {
-    fprintf(stderr, "--- TESTING PACKET ---\nUnused parameters : %i %s\n", argc-1, argv[1]);
-
-    pkt_t *pkt = pkt_new();
-    pkt_set_type(pkt, PTYPE_DATA);
-    pkt_set_tr(pkt, 0);
-    pkt_set_window(pkt, 31);
-    pkt_set_seqnum(pkt, 255);
-    //pkt_set_length(pkt, strlen("zzabcdefzz")); // done in set_payload
-    pkt_set_timestamp(pkt, (uint32_t) 4294967294);
-    //uint32_t crc1 = crc32(crc32(0L, Z_NULL, 0), (const Bytef *) pkt+0, 8);
-    //pkt_set_crc1(pkt, crc1);
-    pkt_set_payload(pkt, "zzabcdefzz", strlen("zzabcdefzz")+1);
-    //uint32_t crc2 = crc32(crc32(0L, Z_NULL, 0), (const Bytef *) pkt+12, pkt_get_length(pkt));
-    //pkt_set_crc2(pkt, crc2);
-
-    size_t len = 512;
-    char *buf = malloc(len);
-    //buf[511] = '\0';
-    pkt_encode(pkt, buf, &len); //len-Post nombre octets écrits
-
-    pkt_del(pkt);
-    pkt = pkt_new();
-
-    pkt_decode(buf, len, pkt);
-    fprintf(stderr, "TypeTrWin : %02x\n", (pkt_get_type(pkt)<<6)+(pkt_get_tr(pkt)<<5)+pkt_get_window(pkt));
-    fprintf(stderr, "Seqnum    : %02x\n", pkt_get_seqnum(pkt));
-    fprintf(stderr, "Length    : %04x\n", pkt_get_length(pkt));
-    fprintf(stderr, "Timestamp : %08x\n", pkt_get_timestamp(pkt));
-    fprintf(stderr, "CRC1      : %08x\n", pkt_get_crc1(pkt));
-    fprintf(stderr, "Payload   : %s\n", pkt_get_payload(pkt));
-    fprintf(stderr, "CRC2      : %08x\n", pkt_get_crc2(pkt));
-
-    return 0;
-}
-
 pkt_status_code pkt_decode(const char *data, const size_t len, pkt_t *pkt) {
-    fprintf(stderr, "HEX DECODE : " );
-    int i;
-    for (i =0 ; i<(int) len; i++ ) {
-        fprintf(stderr, " %02x ", (unsigned char) *(data+i));
-    }
-    fprintf(stderr, "\n" );
-
-
     if(data == NULL || len == 0) {
         return E_UNCONSISTENT;
     }
@@ -276,13 +226,6 @@ pkt_status_code pkt_encode(const pkt_t *pkt, char *buf, size_t *len) {
     }
 
     *len = (size_t) charWritten; // indiquer le nombre de chars ecrits
-
-    fprintf(stderr, "HEX ENCODE : " );
-    int i;
-    for(i = 0 ; i < (int) *len ; i++ ) {
-        fprintf(stderr, " %02x ", (unsigned char) *(buf+i));
-    }
-    fprintf(stderr, "\n" );
 
     return PKT_OK;
 }
