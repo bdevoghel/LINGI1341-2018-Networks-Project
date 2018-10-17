de1892e6dfa0c39ab36007e10b3313d0b9771c35
Brieuc de Voghel <bdev@telenet.be>
2018-10-17 14:15:12 +0200

    Docsting and argument reading

--- src/receiver.c
+++ src/receiver.c
@@ -15,16 +15,67 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+// TODO ensure no memory leakage !!
+
 /**
- * main function
+ * receiver permet de de realiser un transfer de donnees unidirectionnel et fiable
  *
- * @param argc
- * @param argv
- * @return
+ * utilisation : "receiver hostname port [-f X]"
+ * il accepte la connexion depuis l'adresse [hostname] (:: pour toues les interfaces) avec le port [port] sur lequel il ecoute
+ * si -f est present, les donnees seront imprimees sur le fichier X, sinon elles seront affichees sur stdout
+ *
+ * @param argc : 3 < argc < 5
+ * @param argv : receiver hostname port [-f X]
+ * @return : EXIT_SUCCESS si success
+ *           EXIT_FAILURE si erreur d'execution OU arguments non coherents
+ *           TODO enum erreurs possibles
  */
 int main(int argc, char *argv[]) {
+    /*
+     * Reading arguments
+     */
+    char *hostname;
+    int port;
+    int fOption = 0;
+    char *fileToWrite;
 
-    /* recuperation des arguments */
+    int opt;
+    while((opt = getopt(argc, argv, "f:")) != -1) {
+        switch(opt) {
+            case 'f' :
+                fOption = 1;
+                fileToWrite = optarg;
+                break;
+            default : // unknown option
+                fprintf(stderr, "Unknown argument detected : %s\n", optarg);
+                return EXIT_FAILURE;
+        }
+    }
+
+    int i = 1;
+    int hostnameSet = 0;
+    int portSet = 0;
+    while(i < argc) {
+        if(!strcmp(argv[i], "-f")) {
+            i++;
+        } else if(!hostnameSet) {
+            hostname = argv[i];
+            hostnameSet = 1;
+        } else {
+            port = atoi(argv[i]);
+            portSet = 1;
+        }
+        i++;
+    }
+
+    if(argc > (3 + fOption*2) || !hostnameSet || !portSet) {
+        fprintf(stderr, "%i option(s) read. Usage : \"receiver hostname port [-f X]\"\n", (3 + fOption*2));
+        return EXIT_FAILURE;
+    }
+
+    if(!fOption) {
+        fileToWrite = NULL; // TODO write in stdout
+    }
 
     /* formation du lien entre le sender et le receiver */
 
--- src/sender.c
+++ src/sender.c
@@ -15,41 +15,68 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+// TODO ensure no memory leakage !!
+
 /**
- * main function
+ * sender permet de de realiser un transfer de donnees unidirectionnel et fiable
  *
- * @param argc
- * @param argv
- * @return
+ * utilisation : "sender hostname port [-f X]"
+ * il envoie vers l'adresse [hostname] sur laquelle ecoute le receiver avec le port [port] sur lequel le receiver a ete lance
+ * si -f est present, les donnees seront recuperees du fichier X, sinon elles le seront depuis stdin
+ *
+ * @param argc : 3 < argc < 5
+ * @param argv : sender hostname port [-f X]
+ * @return : EXIT_SUCCESS si success
+ *           EXIT_FAILURE si erreur d'execution OU arguments non coherents
+ *           TODO enum erreurs possibles
  */
 int main(int argc, char *argv[]) {
-
     /*
-     * Recuperation des arguments
+     * Reading arguments
      */
+    char *hostname;
+    int port;
     int fOption = 0;
+    char *fileToRead;
+
     int opt;
-    int nOpt = 0;
-    while((opt = getopt(argc, argv, "f:")) != -1) { // voir man getopt TODO verifier options
+    while((opt = getopt(argc, argv, "f:")) != -1) {
         switch(opt) {
             case 'f' :
                 fOption = 1;
-                nOpt++;
+                fileToRead = optarg;
                 break;
-            default : // option inconnue
-                fprintf(stderr, "Unknown argument detected\n");
+            default : // unknown option
+                fprintf(stderr, "Unknown argument detected : %s\n", optarg);
                 return EXIT_FAILURE;
         }
     }
-    if(argc - 1 == nOpt) { // default
-        fprintf(stderr, "%i option(s) read\n", nOpt);
-    } else {
-        fprintf(stderr, "%i option(s) read\n", argc - 1);
-        // TODO handle other arguments
+
+    int i = 1;
+    int hostnameSet = 0;
+    int portSet = 0;
+    while(i < argc) {
+        if(!strcmp(argv[i], "-f")) {
+            i++;
+        } else if(!hostnameSet) {
+            hostname = argv[i];
+            hostnameSet = 1;
+        } else {
+            port = atoi(argv[i]);
+            portSet = 1;
+        }
+        i++;
     }
 
+    if(argc > (3 + fOption*2) || !hostnameSet || !portSet) {
+        fprintf(stderr, "%i option(s) read. Usage : \"receiver hostname port [-f X]\"\n", (3 + fOption*2));
+        return EXIT_FAILURE;
+    }
 
-    fprintf(stderr, "fOption : %i\n", fOption);
+    if(!fOption) {
+        fileToRead = NULL; // TODO read stdin
+    }
+
 
     /* ouverture du fichier */
 
