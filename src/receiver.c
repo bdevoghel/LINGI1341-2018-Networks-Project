30d89a1f4baaa5da02431c5b6f59d065cd5bf6c9
Brieuc de Voghel <bdev@telenet.be>
2018-10-28 02:43:18 +0200

    Stupidity removal

--- src/receiver.c
+++ src/receiver.c
@@ -111,13 +111,6 @@ int main(int argc, char *argv[]) {
 
     fprintf(stderr, "Packets received successfully.\n");
 
-    /* TODO good idea ? wait for read to fail (sfd closed by sender);
-    size_t justRead = 1;
-    while((int)justRead >= 0) {
-        justRead = (size_t) read(socketFileDescriptor, fileToWrite, 1);
-    }
-     */
-
     close(socketFileDescriptor);
     close(outputFileDescriptor);
 
